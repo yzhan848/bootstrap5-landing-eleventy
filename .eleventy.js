442800ebe42514934284318839b25f6f5550d1c2
雅文 <37919609+yzhan848@users.noreply.github.com>
2025-12-01 01:48:06 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets");
+  // 確保 assets 會被複製到 _site/assets 底下
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
 
   return {
+    // ⚠️ 這裡要填你的 repo 名稱（如果 repo 名稱不同要改）
+    pathPrefix: "/bootstrap5-landing-eleventy/",
     dir: {
       input: "src",
       includes: "_includes",
